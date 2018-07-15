b02b9dcd3621014fa6bb4c3f6c5cd7a4fe72e253
foreverwb <504543582@qq.com>
2018-07-15 21:03:02 +0800

    login

--- src/App.js
+++ src/App.js
@@ -2,26 +2,42 @@ import React, { Component } from 'react';
 import './App.css';
 import {Provider} from 'react-redux';
 import store from './data/create';
+import {Router, Route} from 'react-router';
+import history from './history/history';
 
 import SignUp from './components/sign/signup';
+import Login from './components/sign/login';
 
 class App extends Component {
   render() {
     return (
       <div className="main">
           <SignUp/>
+          {/* {this.props.children} */}
       </div>
     );
   }
 }
 
+/**
+ * 
+ *  #hash
+ *  /
+ *  /login -> 登录页面
+ *  /signup -> 注册页面
+ *  /chat -> 聊天页面
+ *  /chat/single/:uid -> 单聊
+ *  /chat/group/:uid -> 群聊
+ */
 export default class Main extends Component {
     render() {
       return (
         <Provider store = {store}>
-            <div>
-                <App />
-            </div>
+            <Router history = {history}>
+                <Route path="/" component={App}></Route>
+                <Route path="/signup" component={SignUp}></Route>
+                <Route path="/login" component={Login}></Route>
+            </Router>
         </Provider>
       );
     }
---
+++ src/components/sign/login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import classnames from 'classnames';
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>login</div>
+        );
+    }
+}
