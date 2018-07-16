fa7c2c2ca39acae24d9fc8ea44b27ca8eb1e8d7c
wangbin <wangbin01@btzh.cn>
2018-07-16 17:38:39 +0800

    login temp

--- src/components/sign/login.js
+++ src/components/sign/login.js
@@ -1,11 +1,73 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router';
 
+import {login} from '../../data/actions/sign';
 import classnames from 'classnames';
+import tooltip from '../common/tooltip';
 
+@connect(
+    (state) => ({}),
+    {
+        login
+    }
+)
 export default class Login extends Component {
+
+    login = () => {
+        let {login} = this.props;
+        let userName = this.refs.name.value;
+        let pwd = this.refs.auth.value;
+        userName = userName.trim();
+        pwd = pwd.trim();
+        if (!userName || !pwd) {
+            tooltip.show({
+                type: 'error',
+                content: '账号密码不能为空'
+            });
+            return;
+        }
+        let options = {
+            apiUrl: WebIM.config.apiURL,
+            appKey: WebIM.config.appkey,
+            user: userName,
+            pwd: pwd
+        };
+        login(options).then((token) => {
+            console.log(token)
+        }).catch(() => {
+            tooltip.show({
+                type: 'error',
+                content: '登录失败'
+            });
+        });
+    }
     render() {
+        let mainClassName = classnames({
+            'sign': true,
+            'login': true
+        })
         return (
-            <div>login</div>
+            <div className={mainClassName}>
+                <h2>登录页面</h2>
+                <input ref='name' 
+                    name="name" 
+                    className="input"
+                    placeholder='输入用户名' 
+                    autoFocus={true} 
+                    />
+                <input ref='auth' 
+                    name="auth" 
+                    className="input"
+                    placeholder='输入密码' 
+                    type='password' 
+                    />
+
+                <button className="button" onClick={this.login}>登录</button>
+                <p>没有账号,
+                    <Link to="/signup">请注册 </Link>
+                </p>
+            </div>
         );
     }
 }
